82ca49600c054e69981bdeddb0b5a5b4b15f4741
mouaadh11 <mouaadhsahailia@gmail.com>
2026-05-17 16:31:04 +0200

    Fix sticky left sidebar alignment

--- src/components/left-side.tsx
+++ src/components/left-side.tsx
@@ -3,7 +3,7 @@ import StickyCard from "@/components/ui/sticky-card";
 
 export function Left() {
   return (
-    <div className="flex w-full lg:sticky lg:top-10 lg:w-[344px] lg:shrink-0">
+    <div className="flex w-full lg:sticky lg:top-10 lg:h-fit lg:w-[344px] lg:shrink-0 lg:self-start">
       <StickyCard />
     </div>
   );
